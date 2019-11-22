a44e8f4e620f7c5861800e712c9fdf940547d9db
xzwdev <dev5@qq.com>
2019-11-22 10:58:07 +0800

    dev5

---
+++ XmsgImAuth.h
@@ -0,0 +1,38 @@
+/*
+  Copyright 2019 www.dev5.cn, Inc. dev5@qq.com
+ 
+  This file is part of X-MSG-IM.
+ 
+  X-MSG-IM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  X-MSG-IM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU Affero General Public License
+  along with X-MSG-IM.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef XMSGIMAUTH_H_
+#define XMSGIMAUTH_H_
+
+#include <libx-msg-im-auth-core.h>
+
+class XmsgImAuth
+{
+public:
+	bool start(const char* path); 
+	static XmsgImAuth* instance();
+private:
+	static XmsgImAuth* inst;
+	bool connect2ne(shared_ptr<XscTcpServer> tcpServer); 
+	static void job(ullong now); 
+	XmsgImAuth();
+	virtual ~XmsgImAuth();
+};
+
+#endif 
